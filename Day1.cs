4c05390d983c6e446bbff1d1f0bdfd3ba6076a17
CameronC127 <cameron.cronkhite@gmail.com>
2022-12-02 09:50:30 -0800

    Day1 complete

---
+++ Day1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode2022
+{
+  internal class Day1
+  {
+    public void Run()
+    {
+      List<int> elfCals = new List<int>();
+      int curCalCount = 0;
+      string[] lines = System.IO.File.ReadAllLines(InputFilePath);
+      foreach (string line in lines)
+      {
+        try
+        {
+          curCalCount += int.Parse(line);
+        }
+        catch (Exception e)
+        {
+          elfCals.Add(curCalCount);
+          curCalCount = 0;
+        }
+      }
+
+      int bestElf = elfCals.Max();
+      Console.WriteLine($"Best Elf: {bestElf}");
+      elfCals.Remove(bestElf);
+
+      int secondBestElf = elfCals.Max();
+      Console.WriteLine($"Second Best Elf: {secondBestElf}");
+      elfCals.Remove(secondBestElf);
+
+      int thirdBestElf = elfCals.Max();
+      Console.WriteLine($"Third Best Elf: {thirdBestElf}");
+      elfCals.Remove(thirdBestElf);
+
+      int totalElfs = bestElf + secondBestElf + thirdBestElf;
+      Console.WriteLine($"Total Cals: {totalElfs}");
+    }
+    public string InputFilePath { get; set; } = @"C:\Users\Cameron Cronkhite\OneDrive\git\AdventOfCode2022\day1input.txt";
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2022;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Advent of Code 2022!");
+Day1 day1 = new Day1();
+day1.Run();
+
